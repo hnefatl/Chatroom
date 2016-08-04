5d8a3246162d87d03c5566d5a562bcfa41a11ec4
Keith Collister <hnefatl@gmail.com>
2016-08-04 16:37:06 +0100

    Working on standard networking features

---
+++ Core/src/User.cpp
@@ -0,0 +1,16 @@
+#include "User.h"
+
+User::User()
+{
+
+}
+
+void User::Serialise(std::vector<unsigned char> &Buffer) const
+{
+
+
+}
+std::size_t User::Deserialise(const std::vector<unsigned char> &Buffer, const std::size_t Start)
+{
+
+}
