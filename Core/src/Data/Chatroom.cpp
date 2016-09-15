181dc72aae22fd3cf317371117a8543ba8646376
Keith Collister <hnefatl@gmail.com>
2016-09-15 13:44:22 +0100

    Fixed all windows build errors

--- Core/src/Data/Chatroom.cpp
+++ Core/src/Data/Chatroom.cpp
@@ -2,17 +2,17 @@
 
 namespace Data
 {
-	extern const std::string Chatroom::CreationString =	"CREATE TABLE Chatroom ("
-														"  Name TEXT NOT NULL,"
-														"  OwnerUsername TEXT NOT NULL,"
-														"  ServerIP TEXT NOT NULL,"
-														"  ServerPort INTEGER NOT NULL,"
-														"  ServerFamily INTEGER NOT NULL,"
-														"  Password TEXT,"
-														"  Description TEXT,"
-														""
-														"  PRIMARY KEY (Name),"
-														"  FOREIGN KEY (OwnerUsername) REFERENCES User,"
-														"  FOREIGN KEY (ServerIP, ServerPort) REFERENCES Server"
-														");";
+	const std::string Chatroom::CreationString =	"CREATE TABLE Chatroom ("
+													"  Name TEXT NOT NULL,"
+													"  OwnerUsername TEXT NOT NULL,"
+													"  ServerIP TEXT NOT NULL,"
+													"  ServerPort INTEGER NOT NULL,"
+													"  ServerFamily INTEGER NOT NULL,"
+													"  Password TEXT,"
+													"  Description TEXT,"
+													""
+													"  PRIMARY KEY (Name),"
+													"  FOREIGN KEY (OwnerUsername) REFERENCES User,"
+													"  FOREIGN KEY (ServerIP, ServerPort) REFERENCES Server"
+													");";
 }
