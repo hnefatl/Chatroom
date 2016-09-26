f582562256cf387de0d9ba28418985fc80c06eb0
Keith Collister <hnefatl@gmail.com>
2016-09-26 15:44:21 +0100

    Pre-switch to virtual machine

--- Core/include/Net/Client/ServerConnection.h
+++ Core/include/Net/Client/ServerConnection.h
@@ -1,6 +1,9 @@
 #ifndef _SERVERCONNECTION_H
 #define _SERVERCONNECTION_H
 
+#include "Net/Net.h"
+#include "Net/Address.h"
+
 namespace Net
 {
 	namespace Client
@@ -8,7 +11,7 @@ namespace Net
 		class ServerConnection
 		{
 		protected:
-
+			SOCKET
 
 		public:
 
